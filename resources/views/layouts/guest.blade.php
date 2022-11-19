1c3ec45c31caffae572f606c32988705ca2a368b
tungnt1405 <ntt140520.bk@gmail.com>
2022-11-19 23:21:41 +0700

    remove lib ckeditor

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        @vite(['resources/scss/main.scss', 'resources/js/app.js'])
-    </head>
-    <body>
-        {{ $slot }}
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    @vite(['resources/scss/main.scss', 'resources/js/app.js'])
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
 </html>
