ffbc9c7c85be0a9a82d150967b303e19ceaa21e3
tungnt1405 <ntt140520.bk@gmail.com>
2023-02-04 23:28:36 +0700

    base design

--- resources/views/livewire/admin/side-bar/more-info.blade.php
+++ resources/views/livewire/admin/side-bar/more-info.blade.php
@@ -5,15 +5,15 @@
             <!-- head -->
             <thead>
                 <tr>
-                    <th>Avatar</th>
-                    <th>Name</th>
-                    <th>Infomation</th>
-                    <th>Facebook</th>
-                    <th>Gmail</th>
-                    <th>Twitter</th>
-                    <th>Linkin</th>
-                    <th>Zalo</th>
-                    <th>Github</th>
+                    <th>@lang('Avatar')</th>
+                    <th>@lang('Name')</th>
+                    <th>@lang('Description')</th>
+                    <th>@lang('Facebook')</th>
+                    <th>@lang('Gmail')</th>
+                    <th>@lang('Twitter')</th>
+                    <th>@lang('Linkin')</th>
+                    <th>@lang('Zalo')</th>
+                    <th>@lang('Github')</th>
                 </tr>
             </thead>
             <tbody>
@@ -33,21 +33,21 @@
                     </td>
                     <td><a class="text-blue-700 hover:text-blue-700/50" href="{{ route('admin.side-bar') }}">Xem chi
                             tiết</a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->fb_url !!}">Facebook<a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->gmail_url !!}">Gmail<a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->twitter_url !!}">Twitter<a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->linkin_url !!}">Linkin<a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->zalo_url !!}">Zalo<a></td>
-                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->github_url !!}">GitHub</a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->fb_url ?: 'javascript:void(-1)' !!}">Facebook<a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->gmail_url ?: 'javascript:void(-1)' !!}">Gmail<a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->twitter_url ?: 'javascript:void(-1)' !!}">Twitter<a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->linkin_url ?: 'javascript:void(-1)' !!}">Linkin<a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->zalo_url ?: 'javascript:void(-1)' !!}">Zalo<a></td>
+                    <td><a class="text-blue-700 hover:text-blue-700/50" href="{!! $owner->github_url ?: 'javascript:void(-1)' !!}">GitHub</a></td>
                 </tr>
             </tbody>
         </table>
     </div>
     <div>
         <h1>Thông tin bổ sung</h1>
-        <div class="md:grid md:grid-cols-3 md:gap-6">
+        <div>
             <x-section.section-title>
-                <x-slot name="title">{{ __('Description') }}</x-slot>
+                <x-slot name="title"></x-slot>
                 <x-slot name="description">{{ __('title.description.sub') }}</x-slot>
             </x-section.section-title>
             <div class="mt-5 md mt-0 md:col-span-2">
