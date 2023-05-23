0144697cef1a52212c98fd14843ce5fd09e54b61
tungnt1405 <ntt140520.bk@gmail.com>
2023-05-23 23:50:33 +0700

    update

--- lang/en/client/validation.php
+++ lang/en/client/validation.php
@@ -2,6 +2,8 @@
 
 return [
     'suggest' => [
-        'not_found' => 'No posts available'
-    ]
+        'not_found' => 'No posts available.',
+        'not_eligible' => ":value value not found."
+    ],
+    'server_error' => 'System error. Please contact the administrator.'
 ];
--- lang/vi/client/validation.php
+++ lang/vi/client/validation.php
@@ -1,6 +1,8 @@
 <?php
 return [
     'suggest' => [
-        'not_found' => 'Không tìm thấy bài viết nào.'
-    ]
+        'not_found' => 'Không tìm thấy bài viết nào.',
+        'not_eligible' => "Không tìm thấy :value."
+    ],
+    'server_error' => 'Lỗi hệ thống. Vui lòng liên hệ với admin.'
 ];
